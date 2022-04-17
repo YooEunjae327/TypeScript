adc72df11bfde28759e40767f1eb43e07e8d1733
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-04-17 18:41:48 +0900

    .

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // array
-let a: string[] = ["kim", "park"]
+let a: string[] = ['kim', 'park']
 
 // object
-let b: { name?: string } = { name: "kim" }
+let b: { name?: string } = { name: 'kim' }
 
 // Union type
 let c: string | number = 123
@@ -16,3 +16,6 @@ let d: D1 = 123
 function fuc(x: number): number {
   return x * 2
 }
+
+let hello = 'kim'
+let age = 12
