823b9c5df3bd05987fccfbc4c4a86ecf79f4c26d
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-04-24 19:22:17 +0900

    .

--- index.ts
+++ index.ts
@@ -1,21 +1,34 @@
-// array
-let a: string[] = ['kim', 'park']
-
-// object
-let b: { name?: string } = { name: 'kim' }
-
-// Union type
-let c: string | number = 123
-
-// Type alias
-type D1 = string | number
-
-let d: D1 = 123
-
-// function
-function fuc(x: number): number {
-  return x * 2
+interface a {
+  name: string
 }
 
-let hello = 'kim'
-let age = 12
+interface b extends a {
+  age: 20
+}
+
+const c: b = {
+  name: 'hi',
+  age: 20,
+}
+
+// // array
+// let a: string[] = ['kim', 'park']
+
+// // object
+// let b: { name?: string } = { name: 'kim' }
+
+// // Union type
+// let c: string | number = 123
+
+// // Type alias
+// type D1 = string | number
+
+// let d: D1 = 123
+
+// // function
+// function fuc(x: number): number {
+//   return x * 2
+// }
+
+// let hello = 'kim'
+// let age = 12
