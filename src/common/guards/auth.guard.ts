40aa3c61ec02b119cd2c024eee1a774d6f804754
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-15 20:31:44 +0900

    .

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,29 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+// import { IToken } from 'src/share/interfaces/IToken';
+// import { validationNullORUndefined } from 'src/share/utils/validation.util';
+// import { TokenService } from 'src/token/token.service';
+// import { User } from 'src/user/entities/user.entity';
+// import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class AuthGuard implements CanActivate {
+  constructor(
+    private readonly userService: UserService,
+    private readonly tokenService: TokenService,
+  ) {}
 
-// import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-// import { Observable } from 'rxjs';
-// import { validationNullORundefined } from 'src/share/utils/validation.util';
-// import { TokenService } from 'src/token/token.service';
-// import { UserService } from 'src/users/users.service';
+  public async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
 
-// @Injectable()
-// export class AuthGuard implements CanActivate {
-//   constructor(
-//     private readonly userService: UserService,
-//     private readonly tokenService: TokenService,
-//   ) {}
+    const token: string = req.headers['authorization'];
+    if (validationNullORUndefined(token)) {
+      throw new BadRequestException('토큰이 없어요');
+    }
 
-//   public async canActivate(context: ExecutionContext): Promise<boolean> {
-//     const req = context.switchToHttp().getRequest();
+    const payload: IToken = await this.tokenService.verifyToken(token);
+    const user: User | undefined = await this.userService.getUserByUserID(
+      payload.userId,
+    );
 
-//     console.log(req);
+    if (validationNullORUndefined(user)) {
+      throw new UnauthorizedException('유저가 존재하지 않아요');
+    }
 
-//     return true;
-//   }
-// }
-
-console.log();
+    req.user = user;
+    return true;
+  }
+}
