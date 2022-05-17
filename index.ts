065a71f4c942fdcecf2fee44dcf191bfd4c71a5c
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-18 08:16:42 +0900

    .

--- index.ts
+++ index.ts
@@ -1,33 +1,34 @@
-class a {
-  name: string;
-  age: number;
-  email: string;
-
-  constructor(name: string, age: number, email: string) {
-    this.name = name;
-    this.age = age;
-    this.email = email;
-  }
-
-  draw() {
-    console.log(
-      `이름은 ${this.name}, 나이는 ${this.age}, 이메일은 ${this.email}입니다.`,
-    );
-  }
+/* eslint-disable no-var */
+for (var i = 0; i < 10; i++) {
+  console.log(i);
 }
 
-class b extends a {
-  name: string;
-  constructor(name: string, age: number, email: string, name2: string) {
-    super(name, age, email);
-    this.name = name2;
-  }
+// class a {
+//   name: string;
 
-  print() {
-    console.log(this.name, super.name());
-  }
-}
+//   constructor(name: string) {
+//     this.name = name;
+//   }
 
-const testing = new b('홍길동', 20, 'tomato@gmail.com', 'hello');
+//   draw() {
+//     console.log(`sd`);
+//   }
+// }
 
-testing.print();
+// class b extends a {
+//   nameh: string;
+//   constructor(name: string, name2: string) {
+//     super(name);
+//     this.nameh = name2;
+//   }
+
+//   print() {
+//     console.log(this.name, this.name);
+//   }
+// }
+
+// const testing = new b('홍길동', 'hello');
+
+// testing.print();
+
+// testing.draw()
