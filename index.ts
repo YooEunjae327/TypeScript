b8aaea1ed44703bedc45ff7c66aa19c29113332b
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-17 09:34:01 +0900

    .

--- index.ts
+++ index.ts
@@ -1,18 +1,30 @@
-class to {
-  a1: string;
-  a2: string;
-  name: string;
+// class to {
+//   a1: string;
+//   a2: string;
+//   name: string;
 
-  constructor(a1: string, a2: string) {
-    this.a1 = a1;
-    this.a2 = a2;
-  }
+//   constructor(a1: string, a2: string) {
+//     this.a1 = a1;
+//     this.a2 = a2;
+//   }
 
-  print() {
-    console.log(this.a1, this.a2);
-  }
+//   async print() {
+//     await to;
+//   }
+// }
+
+// to.prototype.name = '1';
+
+// console.log(to.prototype);
+// console.log(to.apply);
+
+class a {
+  b: string;
+  sex: string;
 }
 
-to.prototype.name = '1';
+a.prototype.sex = '1';
 
-console.log(to.prototype);
+const i = new a();
+
+console.log(i.sex);
