cb6ac1c95bc18504d3ebf82b8c8b6e8125a69411
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-17 20:00:29 +0900

    .

--- index.ts
+++ index.ts
@@ -1,30 +1,33 @@
-// class to {
-//   a1: string;
-//   a2: string;
-//   name: string;
-
-//   constructor(a1: string, a2: string) {
-//     this.a1 = a1;
-//     this.a2 = a2;
-//   }
-
-//   async print() {
-//     await to;
-//   }
-// }
-
-// to.prototype.name = '1';
-
-// console.log(to.prototype);
-// console.log(to.apply);
-
 class a {
-  b: string;
-  sex: string;
+  name: string;
+  age: number;
+  email: string;
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+
+  draw() {
+    console.log(
+      `이름은 ${this.name}, 나이는 ${this.age}, 이메일은 ${this.email}입니다.`,
+    );
+  }
 }
 
-a.prototype.sex = '1';
+class b extends a {
+  name: string;
+  constructor(name: string, age: number, email: string, name2: string) {
+    super(name, age, email);
+    this.name = name2;
+  }
 
-const i = new a();
+  print() {
+    console.log(this.name, super.name());
+  }
+}
 
-console.log(i.sex);
+const testing = new b('홍길동', 20, 'tomato@gmail.com', 'hello');
+
+testing.print();
