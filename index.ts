80d852a402d93d44026e05de463efc732a020570
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-18 21:02:08 +0900

    .

--- index.ts
+++ index.ts
@@ -1,34 +1,15 @@
-/* eslint-disable no-var */
-for (var i = 0; i < 10; i++) {
-  console.log(i);
+class a {
+  //status: number;
+  message: string;
+
+  constructor(message: string) {
+    //this.status = status;
+    this.message = message;
+  }
+
+  static success(message: string) {
+    return new a(message);
+  }
 }
 
-// class a {
-//   name: string;
-
-//   constructor(name: string) {
-//     this.name = name;
-//   }
-
-//   draw() {
-//     console.log(`sd`);
-//   }
-// }
-
-// class b extends a {
-//   nameh: string;
-//   constructor(name: string, name2: string) {
-//     super(name);
-//     this.nameh = name2;
-//   }
-
-//   print() {
-//     console.log(this.name, this.name);
-//   }
-// }
-
-// const testing = new b('홍길동', 'hello');
-
-// testing.print();
-
-// testing.draw()
+console.log(a.success('sdsa'));
