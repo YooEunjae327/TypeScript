b8f921f00abf29cf0798878d74aecfae028239bd
aaaaaa <91647398+YooEunjae327@users.noreply.github.com>
2022-05-24 08:20:41 +0900

    .

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
-class a {
-  //status: number;
-  message: string;
+// class a {
+//   //status: number;
+//   message: string;
 
-  constructor(message: string) {
-    //this.status = status;
-    this.message = message;
-  }
+//   constructor(message: string) {
+//     //this.status = status;
+//     this.message = message;
+//   }
 
-  static success(message: string) {
-    return new a(message);
-  }
+//   static success(message: string) {
+//     return new a(message);
+//   }
+// }
+
+// console.log(a.success('sdsa'));
+
+// console.error(e);
+
+console.log('sry');
+console.log('sryhihi');
+
+const a =
+  'insert into student(st_num, st_numc, st_class, st_number,st_name, st_address) values (2101, 2, 1, 1, "강보민", "대구");';
+
+for (let i = 1; i <= 19; i++) {
+  console.log(
+    `insert into student(st_num, st_numc, st_class, st_number,st_name, st_address) values (210${i}, 2, 1, ${i}, "강보민", "대구");`,
+  );
 }
-
-console.log(a.success('sdsa'));
-
-console.error(e);
-console.lopg(e)
-console.log('sry')
-console.log('sryhihi')
